558e62931e8643cf042de6c7c4348532746a4200
princej24 <116045993+princej24@users.noreply.github.com>
2024-02-12 16:38:21 -0800

    done

--- GALinkedListDoubly_princeM/DoublyLinkedList.cs
+++ GALinkedListDoubly_princeM/DoublyLinkedList.cs
@@ -3,12 +3,266 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using static GALinkedListDoubly_princeM.DoublyLinkedList<T>;
 
 namespace GALinkedListDoubly_princeM
 {
-    internal class DoublyLinkedList
+    public class DoublyLinkedList<T>
     {
+        public class LinkedListNode
+        {
+            public T Value { get; internal set; }
+            public LinkedListNode Next { get; internal set; }
+            public LinkedListNode Previous { get; internal set; }
 
+            public LinkedListNode(T value)
+            {
+                Value = value;
+            }
+        }
 
+        private LinkedListNode _head;
+        private LinkedListNode _tail;
+        private int _count;
+
+        public int Count => _count;
+
+        // Constructor
+        public DoublyLinkedList()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+
+        public void Add(T value)
+        {
+            LinkedListNode newNode = new LinkedListNode(value);
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = _tail;
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+            _count++;
+        }
+
+        public void DisplayForward()
+        {
+            LinkedListNode current = _head;
+            while (current != null)
+            {
+                Console.Write(current.Value + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public void DisplayBackward()
+        {
+            LinkedListNode current = _tail;
+            while (current != null)
+            {
+                Console.Write(current.Value + " ");
+                current = current.Previous;
+            }
+            Console.WriteLine();
+        }
+
+        public void Remove(T value)
+        {
+            LinkedListNode current = _head;
+            while (current != null)
+            {
+                if (current.Value.Equals(value))
+                {
+                    if (current == _head)
+                    {
+                        _head = current.Next;
+                        if (_head != null)
+                            _head.Previous = null;
+                    }
+                    else if (current == _tail)
+                    {
+                        _tail = current.Previous;
+                        if (_tail != null)
+                            _tail.Next = null;
+                    }
+                    else
+                    {
+                        current.Previous.Next = current.Next;
+                        current.Next.Previous = current.Previous;
+                    }
+                    _count--;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+
+        public void InsertAtIndex(int index, T value)
+        {
+            if (index < 0 || index > _count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (index == 0)
+            {
+                InsertAtFront(value);
+                return;
+            }
+
+            if (index == _count)
+            {
+                Add(value);
+                return;
+            }
+
+            LinkedListNode newNode = new LinkedListNode(value);
+            LinkedListNode current = _head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.Next;
+            }
+
+            newNode.Next = current.Next;
+            newNode.Previous = current;
+            current.Next.Previous = newNode;
+            current.Next = newNode;
+            _count++;
+        }
+
+        
+        public void InsertAtFront(T value)
+        {
+            LinkedListNode newNode = new LinkedListNode(value);
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                newNode.Next = _head;
+                _head.Previous = newNode;
+                _head = newNode;
+            }
+            _count++;
+        }
+
+        public void InsertAtEnd(T value)
+        {
+            Add(value);
+        }
+
+        public void RemoveAtIndex(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (index == 0)
+            {
+                RemoveAtFront();
+                return;
+            }
+
+            if (index == _count - 1)
+            {
+                RemoveAtEnd();
+                return;
+            }
+
+            LinkedListNode current = _head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            current.Previous.Next = current.Next;
+            current.Next.Previous = current.Previous;
+            _count--;
+        }
+
+        public void RemoveAtFront()
+        {
+            if (_head == null)
+                throw new InvalidOperationException("List is empty");
+
+            _head = _head.Next;
+            if (_head != null)
+                _head.Previous = null;
+            else
+                _tail = null;
+            _count--;
+        }
+
+        public void RemoveAtEnd()
+        {
+            if (_tail == null)
+                throw new InvalidOperationException("List is empty");
+
+            _tail = _tail.Previous;
+            if (_tail != null)
+                _tail.Next = null;
+            else
+                _head = null;
+            _count--;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+
+                LinkedListNode current = _head;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                return current.Value;
+            }
+            set
+            {
+                if (index < 0 || index >= _count)
+                    throw new IndexOutOfRangeException();
+
+                LinkedListNode current = _head;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                current.Value = value;
+            }
+        }
     }
+
+
 }
--- GALinkedListDoubly_princeM/Program.cs
+++ GALinkedListDoubly_princeM/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace GALinkedListDoubly_princeM
 {
-    internal class Program
+
+
+    class Program
     {
         static void Main(string[] args)
         {
+        
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+        
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            Console.WriteLine("Displaying elements from head to tail:");
+            list.DisplayForward();
+
+            Console.WriteLine("Displaying elements from tail to head:");
+            list.DisplayBackward();
+
+            list.InsertAtIndex(2, 99);
+            Console.WriteLine("After inserting 99 at index 2 (before tail):");
+            list.DisplayForward();
+
+            list.InsertAtFront(0);
+            Console.WriteLine("After inserting 0 at the front (head):");
+            list.DisplayForward();
+
+            list.InsertAtEnd(5);
+            Console.WriteLine("After inserting 5 at the end (tail):");
+            list.DisplayForward();
+
+            
+            list.Remove(2);
+            Console.WriteLine("After removing element 2:");
+            list.DisplayForward();
+
+          
+            list.RemoveAtEnd();
+            Console.WriteLine("After removing last element (tail):");
+            list.DisplayForward();
+
+            list.Clear();
+            Console.WriteLine("After clearing the list:");
+            list.DisplayForward();
         }
     }
 }
